bfadc3bd82fd6419fcaaa38eb568c87edb82b302
FranSalvatierra16 <103911827+FranSalvatierra16@users.noreply.github.com>
2023-08-01 23:58:16 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const itemsLeft = todos.length;
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { title: newTodo }]);
+      setNewTodo('');
+    }
+  };
+
+  const handleRemoveTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  const handleInputKeyPress = (e) => {
+    if (e.key === 'Enter' && newTodo.trim() !== '') {
+      handleAddTodo();
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>todos</h1>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          onKeyPress={handleInputKeyPress}
+        />
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo.title}
+            <button onClick={() => handleRemoveTodo(index)}>X</button>
+          </li>
+        ))}
+      </ul>
+      <p>{itemsLeft} item{itemsLeft !== 1 ? 's' : ''} left</p>
+    </div>
+  );
 };
 
 export default Home;
