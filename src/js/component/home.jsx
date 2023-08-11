93c69e8aebcb829ed090d66954fa3724849ae200
FranSalvatierra16 <103911827+FranSalvatierra16@users.noreply.github.com>
2023-08-11 03:26:47 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
-  const itemsLeft = todos.length;
+  const itemsLeft = todos.filter(todo => !todo.done).length;
+
+  useEffect(() => {
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/francisco')
+      .then(response => response.json())
+      .then(data => setTodos(data))
+      .catch(error => console.error('Error fetching todos:', error));
+  }, []);
 
   const handleAddTodo = () => {
     if (newTodo.trim() !== '') {
-      setTodos([...todos, { title: newTodo }]);
+      fetch('https://playground.4geeks.com/apis/fake/todos/user/francisco', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          label: newTodo,
+          done: false,
+          username: 'francisco'
+        })
+      })
+      .then(response => response.json())
+      .then(data => setTodos([...todos, data]))
+      .catch(error => console.error('Error adding todo:', error));
+      
       setNewTodo('');
     }
   };
 
+  const handleToggleTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].done = !updatedTodos[index].done;
+
+    fetch(`https://playground.4geeks.com/apis/fake/todos/user/francisco${updatedTodos[index].id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedTodos[index])
+    })
+    .then(response => response.json())
+    .then(data => setTodos(updatedTodos))
+    .catch(error => console.error('Error updating todo:', error));
+  };
+
   const handleRemoveTodo = (index) => {
-    setTodos(todos.filter((_, i) => i !== index));
+    fetch(`https://playground.4geeks.com/apis/fake/todos/user/francisco${todos[index].id}`, {
+      method: 'DELETE'
+    })
+    .then(() => {
+      const updatedTodos = todos.filter((_, i) => i !== index);
+      setTodos(updatedTodos);
+    })
+    .catch(error => console.error('Error removing todo:', error));
   };
 
   const handleInputKeyPress = (e) => {
@@ -35,8 +79,8 @@ const Home = () => {
       </div>
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}>
-            {todo.title}
+          <li key={index} className={todo.done ? 'completed' : ''}>
+            <span onClick={() => handleToggleTodo(index)}>{todo.label}</span>
             <button onClick={() => handleRemoveTodo(index)}>X</button>
           </li>
         ))}
